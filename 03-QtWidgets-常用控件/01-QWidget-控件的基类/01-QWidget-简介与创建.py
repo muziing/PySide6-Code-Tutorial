616e0561a7ff1a622c9991dd7f28f96ac4c7a67a
muzing <muzi2001@foxmail.com>
2022-06-06 10:12:17 +0800

    Add 03-01-01, 03-01-02

---
+++ 03-QtWidgets-常用控件/01-QWidget-控件的基类/01-QWidget-简介与创建.py
@@ -0,0 +1,45 @@
+import sys
+
+from PySide6 import QtWidgets
+
+"""
+QWidget
+官方文档：https://doc.qt.io/qtforpython/PySide6/QtWidgets/QWidget.html
+1.所有可视控件的基类
+2.是一个最简单的空白控件
+3.控件是用户界面的最小元素
+4.每个控件都是矩形的，它们按Z轴顺序排序
+5.控件由其父控件和前面的控件裁剪
+6.没有父控件的控件，称之为窗口
+"""
+
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        # self创建时未传入父控件，故成为窗口
+        self.setWindowTitle("QWidget简介")  # 设置窗口标题
+        self.resize(800, 600)  # 设置大小
+        self.setup_ui()
+
+    def setup_ui(self) -> None:
+        """设置界面"""
+
+        # 创建一个名为red的QWidget
+        red = QtWidgets.QWidget(self)  # red的父控件为self,故不成为单独窗口
+        red.resize(100, 100)
+        red.setStyleSheet("background-color: red;")
+        red.move(300, 100)
+
+        # 创建一个名为green的QWidget
+        green = QtWidgets.QWidget(self)
+        green.resize(100, 100)
+        green.setStyleSheet("background-color: green;")
+        green.move(300, 150)  # 体现QWidget沿z轴绘制，后面的控件可以覆盖前面的控件
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = MyWidget()
+    window.show()
+    sys.exit(app.exec())
---
+++ 03-QtWidgets-常用控件/01-QWidget-控件的基类/02-QWidget-大小尺寸.py
@@ -0,0 +1,73 @@
+import sys
+
+from PySide6 import QtWidgets
+
+"""
+QWidget 提供了丰富的尺寸设置功能，通过代码可以指定其尺寸、最大最小尺寸、获取当前尺寸等
+默认行为：能完整显示其子控件的最小尺寸
+"""
+
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QWidget 大小与位置")
+        self.setup_ui()
+
+    def setup_ui(self) -> None:
+        """设置界面"""
+        self.test_01()
+        self.test_02()
+        self.test_03()
+
+    def test_01(self) -> None:
+        """测试设置尺寸功能"""
+        # 设置尺寸
+        self.resize(800, 600)  # 设置为指定尺寸，单位为像素
+
+        # 最大最小尺寸
+        # self.setMaximumSize(1000, 800)  # 限制最大尺寸，用户拖拽窗口能达到的最大大小
+        # self.setMaximumWidth(1000)  # 亦可分别单独设置最大宽高
+        # self.setMaximumHeight(800)
+        # self.resize(1500, 1000)  # 即使再通过resize设置更大的尺寸，也无效
+
+        # 固定尺寸
+        # self.setFixedSize(500, 500)  # 设置为固定尺寸，用户不再可拖拽改变窗口尺寸
+        # self.setFixedWidth(500)  # 也可以分别单独设置固定的宽高
+        # self.setFixedHeight(500)
+
+    def test_02(self) -> None:
+        """测试大小自动调整功能"""
+        # 注意首先将固定尺寸代码注释掉
+
+        # 设置一个可以通过按钮增长的文本标签,初次阅读代码时本段可暂时忽略
+        btn = QtWidgets.QPushButton("增长文本", self)
+        btn.move(100, 300)
+        label = QtWidgets.QLabel("muzing.top", self)
+        label.move(0, 100)
+
+        def test_slot():
+            """测试用槽函数，增长label内文字"""
+            new_content = label.text() + " muzing.top"
+            label.setText(new_content)
+            label.adjustSize()  # label的大小会根据其内容进行调整
+
+            # ******************
+            self.adjustSize()  # self的大小根据其子控件大小进行调整
+            # ******************
+
+        # 将按钮的点击信号与槽函数绑定，按钮按下则执行槽函数
+        btn.clicked.connect(test_slot)
+
+    def test_03(self) -> None:
+        """测试获取尺寸功能"""
+        print(self.height())
+        print(self.width())
+        print(self.size())  # 返回QSize对象
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = MyWidget()
+    window.show()
+    sys.exit(app.exec())
