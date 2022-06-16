e492ca7d28dc430db6398fdefdad2546d54b39cd
muzing <muzi2001@foxmail.com>
2022-06-16 21:04:26 +0800

    Update 01-01

--- 01-HelloWorld-基本结构/01-HelloWorld-第一个PySide6程序.py
+++ 01-HelloWorld-基本结构/01-HelloWorld-第一个PySide6程序.py
@@ -2,16 +2,21 @@ import random
 import sys
 
 # 导入所需的模块
-from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6 import QtCore, QtWidgets
 
 
 class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)  # 调用父类的初始化方法
 
         self.hello = ["你好世界", "Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        # 设置窗口大小，单位为像素
+        self.resize(800, 600)
+
         # 创建一个按钮控件，其上文字为“点击我”
         self.button = QtWidgets.QPushButton("点击我！")
+
         # 创建一个标签控件，内容为Hello World,对齐方式为居中
         self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
 
@@ -23,7 +28,7 @@ class MyWidget(QtWidgets.QWidget):
         self.layout.addWidget(self.button)
 
         # 将button.clicked这个信号与self.magic槽函数连接
-        self.button.clicked.connect(self.magic)
+        self.button.clicked.connect(self.magic)  # type: ignore
 
     @QtCore.Slot()
     def magic(self) -> None:
@@ -32,10 +37,7 @@ class MyWidget(QtWidgets.QWidget):
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])  # 创建APP
-
+    app = QtWidgets.QApplication(sys.argv)  # 创建APP，将运行脚本时（可能的）的其他参数传给Qt以初始化
     widget = MyWidget()  # 实例化一个MyWidget类对象
-    widget.resize(800, 600)  # 设置窗口大小，单位为像素
     widget.show()  # 显示窗口
-
-    sys.exit(app.exec())  # 正常退出APP
+    sys.exit(app.exec())  # 正常退出APP：app.exec()关闭app，sys.exit()退出进程
