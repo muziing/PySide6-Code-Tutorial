cb775ef23ae5beaa0ffcffcae22d60c934e5fcaa
muzing <muzi2001@foxmail.com>
2022-07-20 09:06:38 +0800

    Add 03-04-01
    QFrame-简介与创建

---
+++ 03-QtWidgets-常用控件/04-QFrame-具边框控件的基类/01-QFrame-简介与创建.py
@@ -0,0 +1,41 @@
+import sys
+
+from PySide6 import QtWidgets
+
+"""
+QFrame 是具有边框的控件的基类
+官方文档：https://doc.qt.io/qtforpython/PySide6/QtWidgets/QFrame.html
+继承自QtWidget
+例如QMenu的“浮起”、QProgressBar的“下沉”、QLabel的“扁平”视觉效果都是来自QFrame
+
+风格与线宽纵览图：https://doc.qt.io/qt-6/images/frames.png
+
+构造函数中可以传入父控件与WindowFlags（见本项目05-03-01）
+.__init__(self, parent: Optional[QWidget] = None, f: Qt.WindowFlags = Default(Qt.WindowFlags))
+
+"""
+
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setWindowTitle("空白测试模板")
+        self.resize(800, 600)
+        self.setup_ui()
+
+    def setup_ui(self) -> None:
+        """设置界面"""
+        frame = QtWidgets.QFrame(self)
+        frame.setStyleSheet("background-color: cyan;")
+        frame.move(200, 200)
+
+        # 设置风格与线宽
+        frame.setFrameStyle(QtWidgets.QFrame.StyledPanel | QtWidgets.QFrame.Sunken)
+        frame.setLineWidth(3)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWidget()
+    window.show()
+    sys.exit(app.exec())
