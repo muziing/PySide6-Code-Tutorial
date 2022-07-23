53c3acc423c6cc783bede73c3a55b0b4368b5745
muzing <muzi2001@foxmail.com>
2022-07-23 10:50:48 +0800

    Update 03-11-01-03
    QComboBox-增加尺寸调整策略

--- 03-QtWidgets-常用控件/11-ComboBox-下拉框/01-QComboBox-组合下拉框/03-QComboBox-条目操作-2.py
+++ 03-QtWidgets-常用控件/11-ComboBox-下拉框/01-QComboBox-组合下拉框/03-QComboBox-条目操作-2.py
@@ -20,6 +20,20 @@ QComboBox 条目操作
 .setInsertPolicy(policy: QComboBox.InsertPolicy)
 .insertPolicy() -> QComboBox.InsertPolicy
 
+尺寸调整策略控制当内容改变时，combobox的尺寸如何改变，QComboBox.SizeAdjustPolicy详情见下文
+.setSizeAdjustPolicy(policy: QComboBox.SizeAdjustPolicy)
+.sizeAdjustPolicy() -> QComboBox.SizeAdjustPolicy
+
+通过控制minimumContentsLength属性，限制最少字符数（默认为0）
+.setMinimumContentsLength(characters: int)
+.minimumContentsLength() -> int
+
+还可以控制combobox中条目的最大数量（默认值为有符号整形的上限）、在屏幕上显示的最大数量（默认为10）
+.setMaxCount(max: int)
+.maxCount() -> int
+.setMaxVisibleItems(max_items: int)
+.maxVisibleItems() -> int
+
 
 QComboBox.InsertPolicy 枚举值具体有如下数种：
 https://doc.qt.io/qt-6/qcombobox.html#InsertPolicy-enum
@@ -31,15 +45,12 @@ QComboBox.InsertAfterCurrent        在当前条目之后插入
 QComboBox.InsertBeforeCurrent       在当前条目之前插入
 QComboBox.InsertAlphabetically      按字母表顺序插入字符串至combobox
 
-通过控制minimumContentsLength属性，限制最少字符数（默认为0）
-.setMinimumContentsLength(characters: int)
-.minimumContentsLength() -> int
-
-还可以控制combobox中条目的最大数量（默认值为有符号整形的上限）、在屏幕上显示的最大数量（默认为10）
-.setMaxCount(max: int)
-.maxCount() -> int
-.setMaxVisibleItems(max_items: int)
-.maxVisibleItems() -> int
+QComboBox.SizeAdjustPolicy 枚举值具体有如下数种：
+https://doc.qt.io/qt-6/qcombobox.html#SizeAdjustPolicy-enum
+QComboBox.AdjustToContents              总是根据内容调整尺寸
+QComboBox.AdjustToContentsOnFirstShow   仅在首次出现时调整尺寸，默认值
+QComboBox.AdjustToMinimumContentsLengthWithIcon
+                                        将尺寸调整至最小内容长度加一个图标，出于性能原因，请在大型模型上使用此策略
 
 """
 
@@ -55,8 +66,7 @@ class MyWidget(QtWidgets.QWidget):
         """设置界面"""
 
         cbb = QtWidgets.QComboBox(self)
-        cbb.move(200, 200)
-        cbb.resize(400, 60)
+        cbb.move(300, 200)
         cbb.addItems([str(i) for i in range(100, 110)])  # 通过列表推导式快速添加多个条目
 
         # 允许用户编辑条目
@@ -76,6 +86,9 @@ class MyWidget(QtWidgets.QWidget):
 
         # 设置可编辑文本
         cbb.setEditText("000")  # 将当前文本设置为00,而不影响其他条目
+
+        # 设置尺寸调整策略
+        cbb.setSizeAdjustPolicy(QtWidgets.QComboBox.AdjustToContents)  # 总是根据内容调整尺寸
 
         # 限制最小字符数
         cbb.setMinimumContentsLength(3)
