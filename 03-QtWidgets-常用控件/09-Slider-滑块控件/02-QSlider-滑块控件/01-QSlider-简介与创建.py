bb779453b7f59c5a9bea8578332813111e79efd7
muzing <muzi2001@foxmail.com>
2022-07-24 22:51:53 +0800

    Add 03-09-02-01
    QSlider-简介与创建

---
+++ 03-QtWidgets-常用控件/09-Slider-滑块控件/02-QSlider-滑块控件/01-QSlider-简介与创建.py
@@ -0,0 +1,37 @@
+import sys
+
+from PySide6 import QtWidgets
+
+"""
+QSlider 滑块控件
+水平或垂直的滑块
+官方文档：https://doc.qt.io/qtforpython/PySide6/QtWidgets/QSlider.html
+继承自QAbstractSlider
+
+相比于其抽象父类QAbstractSlider,主要是添加了刻度线功能
+"""
+
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setWindowTitle("QSlider")
+        self.resize(800, 600)
+        self.setup_ui()
+
+    def setup_ui(self) -> None:
+        """设置界面"""
+
+        slider = QtWidgets.QSlider(self)
+        slider.move(200, 200)
+        slider.resize(40, 200)
+
+        # 在两侧绘制刻度线
+        slider.setTickPosition(QtWidgets.QSlider.TicksBothSides)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWidget()
+    window.show()
+    sys.exit(app.exec())
